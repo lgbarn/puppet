1f946d0cdf06342cb0c85a985c48dc134efd1b6a
Luther Barnum <luther.barnum@gmail.com>
2014-01-05 13:03:08 -0500

    removed br3partest* nodes

--- manifests/site.pp
+++ manifests/site.pp
@@ -12,36 +12,9 @@ node default {
     include role::base
   }
 }
-node "br3partest.ux.corp.local" {
-    class { 'puppetdb::master::config':
-      puppetdb_server => 'br3partest3.ux.corp.local',
-    }
-    class { '::mcollective':
-    client  => true,
-    middleware       => true,
-    middleware_hosts => [ 'br3partest.ux.corp.local' ],
-  }
-}
-node "br3partest2.ux.corp.local" {
-    class { 'puppetdb::database::postgresql':
-      listen_addresses => 'br3partest2',
-    }
-  class { '::mcollective':
-    client  => true,
-    middleware_hosts => [ 'br3partest.ux.corp.local' ],
-  }
-  include role::database::server
-}
-node "br3partest3.ux.corp.local" {
-    class { 'puppetdb::server':
-      database_host => 'br3partest2',
-    }
-  class { '::mcollective':
-    client  => true,
-    middleware_hosts => [ 'br3partest.ux.corp.local' ],
-  }
+node 'web1.ux.corp.local' {
   include role::app::server
 }
-node "web1.ux.corp.local" {
+node 'dbs1.ux.corp.local' {
   include role::database::server
 }
